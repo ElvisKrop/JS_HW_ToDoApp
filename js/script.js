b74d124e2ae04ad79c4c0875a24813cfded5fa0d
Nikita <needsthxxx@gmail.com>
2020-04-07 01:01:49 +0300

    faster

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let inputData = document.querySelector("input[type='text']"),
     aboutBox = document.getElementById("about_box");
 
 function deleteTodo() {
-    for(let span of spans) {
+    for (let span of spans) {
         span.addEventListener("click", () => {
             span.parentElement.remove();
             event.stopPropagation();
@@ -17,35 +17,34 @@ function deleteTodo() {
 }
 
 function lineThrough() {
-    for(let oneLi of eachLi) {
+    for (let oneLi of eachLi) {
         oneLi.addEventListener("click", () => {
-            oneLi.style.textDecoration = 'line-through';
+            oneLi.style.textDecoration = "line-through";
             event.stopPropagation();
         });
     }
 }
 
 function loadTodo() {
-    if(localStorage.getItem('todolist')) {
-        ulSpisok.innerHTML = localStorage.getItem('todolist');
+    if (localStorage.getItem("todolist")) {
+        ulSpisok.innerHTML = localStorage.getItem("todolist");
     }
     deleteTodo();
 }
 
 function closeAboutBox() {
-    aboutBox.addEventListener('click', () => {
-        aboutBox.classList.remove('show-info');
+    aboutBox.addEventListener("click", () => {
+        aboutBox.classList.remove("show-info");
     });
 }
 
-inputData.addEventListener('keypress', function(keyPressed) {
-    if(keyPressed.which === 13 && this.value !="") {
-
+inputData.addEventListener("keypress", function (keyPressed) {
+    if (keyPressed.which === 13 && this.value != "") {
         let liNew = document.createElement("li"),
             spanNew = document.createElement("span"),
             now = new Date();
 
-        spanNew.innerHTML = 'Delete';
+        spanNew.innerHTML = "<i class='far fa-trash-alt'></i>";
 
         let newToDo = `${this.value} (${now.toString().substr(0, 24)});`;
         this.value = "";
@@ -57,16 +56,16 @@ inputData.addEventListener('keypress', function(keyPressed) {
     }
 });
 
-saveBtn.addEventListener('click', function() {
-    localStorage.setItem('todolist', ulSpisok.innerHTML);
+saveBtn.addEventListener("click", function () {
+    localStorage.setItem("todolist", ulSpisok.innerHTML);
 });
 
-clearBtn.addEventListener('click', function() {
+clearBtn.addEventListener("click", function () {
     ulSpisok.innerHTML = "";
-    localStorage.setItem('todolist', ulSpisok.innerHTML);
+    localStorage.setItem("todolist", ulSpisok.innerHTML);
 });
 
-aboutBtn.addEventListener('click', () => {
+aboutBtn.addEventListener("click", () => {
     aboutBox.classList.add("show-info");
 });
 
